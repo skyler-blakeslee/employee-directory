d9c1f5b8d0623d587d0dea5170993731d1f36733
Skyler Blakeslee <skylerblakeslee@gmail.com>
2021-05-10 22:14:38 -0700

    basic page functionality

---
+++ employee-directory/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Placeholder from "./components/Placeholder";
+import './App.css';
+
+function App() {
+  return < Placeholder />;
+
+}
+
+export default App;
---
+++ employee-directory/src/components/Placeholder.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Placeholder() {
+    return <div>
+        <h1>Employee Tracker</h1>
+        <h2>Currently Under Development</h2>;
+    </div>
+}
+
+export default Placeholder;
