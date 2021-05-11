f8374b45ce9b811e3ea23139db79540dcdfd14ca
Skyler Blakeslee <skylerblakeslee@gmail.com>
2021-05-10 22:50:23 -0700

    added basic employees list using json

---
+++ employee-directory/src/components/EmployeeCard.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function EmployeeCard(props) {
+    return (
+        <div className="card">
+            <div className="content">
+                <ul>
+                    <li>
+                        <strong>Name:</strong> {props.lastName} + ", "{props.firstName}
+                    </li>
+                    <li>
+                        <strong>Position:</strong> {props.position}
+                    </li>
+                    <li>
+                        <strong>Start Date:</strong> {props.startDate}
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default EmployeeCard;
