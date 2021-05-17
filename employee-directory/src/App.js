47b216e743988ad1dbf1d29f67c86fc584b3f864
Skyler Blakeslee <skylerblakeslee@gmail.com>
2021-05-17 00:44:22 -0700

    deleting files

--- employee-directory/src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import EmployeeResultContainer from "./components/EmployeeResultContainer";
-
-function App() {
-  return <EmployeeResultContainer />;
-}
-
-export default App;
